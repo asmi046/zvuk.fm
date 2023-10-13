0be0407a4f3d5ce040c07f56b4cdfe5e333c9e0e
asmi046 <asmi046@gmail.com>
2023-10-13 10:21:28 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.all')
+
+@php
+    $title = "zvuk.fm";
+    $description = "zvuk.fm";
+@endphp
+
+@section('title', $title)
+@section('description', $description)
+
+@section('main')
+    <h1>Тест</h1>
+@endsection
+

1cbac5a86928c57d217a866f8c1576215b7a43e6
asmi046 <asmi046@gmail.com>
2023-10-24 09:30:26 +0300

    ПОдвал

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+<footer id="footer" class="footer">
+	<div class="footer__container _container">
+
+		<div class="footer__row d-flex">
+
+			<ul class="footer__menu d-flex">
+				<li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Аудиоролики</a></li>
+				<li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Дикторы</a></li>
+				<li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Хронометраж</a></li>
+				<li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Оплата</a></li>
+				<li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Контакты</a></li>
+			</ul>
+
+			<div class="footer__policy">
+				C 2011 Студия "Эпицентр" - ZVUK.FM Изготовление радиороликов | Радиоролики | Дикторские голоса
+			</div>
+
+		</div>
+
+	</div>
+</footer>
--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -49,7 +49,7 @@
         </section>
 
 
-
+        <x-footer></x-footer>
     </main>
 
 </body>
